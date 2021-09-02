c68bf9c96633787e2afbe0e147453b4a3ee4c0d9
markseeun <mark233891@gmail.com>
2021-09-02 17:07:56 +0900

    clean modules

--- library/src/main/resources/sql/schema.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS Products;
-
-CREATE TABLE Products
-(
-    prod_id     int        PRIMARY KEY auto_increment,
-    prod_name   VARCHAR(255)    NOT NULL,
-    prod_price  INT             NOT NULL
-);
--- plms_start/android/app/src/main/kotlin/com/example/plms_start/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.plms_start
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
