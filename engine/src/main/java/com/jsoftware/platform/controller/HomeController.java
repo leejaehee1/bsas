4b067ee8e95e35c00d84d96b5baa3f97e659e5c9
john <q7phavoc2@gmail.com>
2021-09-05 13:22:45 +0900

    채팅 코드 커밋

--- engine/src/main/java/com/jsoftware/platform/controller/HomeController.java
+++ engine/src/main/java/com/jsoftware/platform/controller/HomeController.java
@@ -1,8 +1,11 @@
 package com.jsoftware.platform.controller;
 
+import com.jsoftware.platform.model.Chat;
+import com.jsoftware.platform.model.ChatRoom;
 import com.jsoftware.platform.vo.DataItem;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +14,8 @@ import javax.servlet.http.HttpSession;
 import javax.sql.DataSource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class HomeController {
@@ -34,6 +39,51 @@ public class HomeController {
     @GetMapping("/home")
     public String findUid(HttpSession session) {
         return session.getId();
+    }
+
+    @CrossOrigin
+    @GetMapping("/chatRooms")
+    public List<ChatRoom> readChatRooms() {
+        List<ChatRoom> chatRooms = new ArrayList<>();
+        ChatRoom chatRoom = new ChatRoom();
+        chatRoom.setContent("my content");
+        chatRoom.setCount("10");
+        chatRoom.setImageUrl("https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fmeeyn%2FbtqIZmZ8EYX%2FxQ2ttsrWljSoJFi30l5wZK%2Fimg.png");
+        chatRoom.setTime("2021-09-03 18:00:00");
+        chatRoom.setTitle("my title");
+        chatRooms.add(chatRoom);
+        System.out.println(chatRoom.toString());
+        return chatRooms;
+    }
+
+    @CrossOrigin
+    @GetMapping("/chats")
+    public List<Chat> readChats() {
+        List<Chat> chats = new ArrayList<>();
+        Chat chat = new Chat();
+        chat.setTime("2021-09-03 18:00:00");
+        chat.setName("It's me.");
+        chat.setText("hello how are you");
+        chat.setType(0);
+        chat.setAvatarUrl("https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fmeeyn%2FbtqIZmZ8EYX%2FxQ2ttsrWljSoJFi30l5wZK%2Fimg.png");
+        chats.add(chat);
+
+        chat = new Chat();
+        chat.setTime("2021-09-03 18:01:00");
+        chat.setName("John");
+        chat.setText("I'm fine.");
+        chat.setType(1);
+        chat.setAvatarUrl("https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fmeeyn%2FbtqIZmZ8EYX%2FxQ2ttsrWljSoJFi30l5wZK%2Fimg.png");
+        chats.add(chat);
+
+        chat = new Chat();
+        chat.setTime("2021-09-03 18:02:00");
+        chat.setName("It's me.");
+        chat.setText("good bye");
+        chat.setType(0);
+        chat.setAvatarUrl("https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fmeeyn%2FbtqIZmZ8EYX%2FxQ2ttsrWljSoJFi30l5wZK%2Fimg.png");
+        chats.add(chat);
+        return chats;
     }
 
     @GetMapping("/test/data")
