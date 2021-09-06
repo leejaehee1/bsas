f492ce622a78d150891ddfedaa2c3703e62b466f
sherrygelato <sherrygelato@kakao.com>
2021-09-06 21:49:18 +0900

    upload 9.6.last

--- engine/src/main/java/com/jsoftware/platform/controller/ActivityConfigController.java
+++ engine/src/main/java/com/jsoftware/platform/controller/ActivityConfigController.java
@@ -59,7 +59,7 @@ public class ActivityConfigController {
 //        return userTest;
 //    }
 
-    // user, hopital, center read one
+    // user, hospital, center read one
     @GetMapping("/api/users/{id}")
     public User readUser(@PathVariable("id") int id) {
         System.out.println(id);
@@ -95,59 +95,38 @@ public class ActivityConfigController {
     }
 
     // user, hospital, center put
-//    @PutMapping("/api/users/{id}")
-//    public User putUser(@PathVariable("id") Long id, @RequestBody User user) {
-//        return activityConfigService.updateUser(user);
-//    }
+    @PutMapping("/api/users/{id}")
+    public User putUser(@PathVariable("id") int id, @RequestBody User user) {
+        return activityConfigService.updateUser(user);
+    }
 
     @PutMapping("/api/hospitals/{id}")
-    public Hospital putHospital(@PathVariable("id") Long id, @RequestBody Hospital hospital) {
+    public Hospital putHospital(@PathVariable("id") int id, @RequestBody Hospital hospital) {
         return activityConfigService.updateHospital(hospital);
     }
 
     @PutMapping("/api/centers/{id}")
-    public Center putCenter(@PathVariable("id") Long id, @RequestBody Center center) {
+    public Center putCenter(@PathVariable("id") int id, @RequestBody Center center) {
         return activityConfigService.updateCenter(center);
     }
 
     // user, hospital, center delete
     @DeleteMapping("/api/users/{id}")
-    public void deleteUser(@PathVariable long id, @RequestBody User user) {
+    public void deleteUser(@PathVariable int id, @RequestBody User user) {
         activityConfigService.deleteUser(user);
     }
 
     @DeleteMapping("/api/hospitals/{id}")
-    public void deleteHospital(@PathVariable long id, @RequestBody Hospital hospital) {
+    public void deleteHospital(@PathVariable int id, @RequestBody Hospital hospital) {
         activityConfigService.deleteHospital(hospital);
     }
 
     @DeleteMapping("/api/centers/{id}")
-    public void deleteCenter(@PathVariable long id, @RequestBody Center center) {
+    public void deleteCenter(@PathVariable int id, @RequestBody Center center) {
         activityConfigService.deleteCenter(center);
     }
 
     // ----------------test
-    // 9월 6일 현재 /api/test/user list와 post, delete는 가능하나, readUser 불가능 상황
-//    @GetMapping("/api/users")
-//    public List<User> readUsers() {
-//        return activityConfigService.readUsers();
-//    }
-//
-//    @GetMapping("/api/users/{id}")
-//    public User readUser(@PathVariable long id, @RequestBody User user) {
-//        return activityConfigService.readUser(user);
-//    }
-//
-//    @PostMapping("/api/users")
-//    public User createUser(@RequestBdy User user) {
-//        return activityConfigService.createUser(user);
-//    }
-//
-//    @PutMapping("/api/users/{id}")
-//    public void updateUser(@PathVariable long id, @RequestBody User user) {
-//        activityConfigService.updateUser(user);
-//    }
-//
 //    @DeleteMapping("/api/users/{id}")
 //    public void deleteUser(@PathVariable long id, @RequestBody User user) {
 //        activityConfigService.deleteUser(user);
