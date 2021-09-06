238a5c03ff25e4eb200583946b847e82ec452667
markseeun <mark233891@gmail.com>
2021-09-06 17:41:29 +0900

    upload

--- flutter/lib/pages/talk_first.dart
+++ flutter/lib/pages/talk_first.dart
@@ -1,7 +1,7 @@
 import 'package:bsas/center/center_home_screen.dart';
 import 'package:bsas/hospital/hospital_home_screen.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
-
 import 'package:bsas/User/user_home_screen.dart';
 
 class TalkPage_first extends StatelessWidget {
@@ -32,38 +32,34 @@ class TalkPage_first extends StatelessWidget {
                   child: Container(
                     margin: EdgeInsets.only(right: 8),
                     padding: EdgeInsets.only(left: 16),
-                    height: 64,
-                    // decoration: BoxDecoration(
-                    //   color: Colors.white,
-                    //   borderRadius: BorderRadius.circular(12),
-                    //   border: Border.all(color: Colors.black12, width: 1),
-                    // ),
-                    child: RaisedButton(
-                      color: Color(0xFF43aa8b),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                      onPressed: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (_) => CenHomeScreen()));
-                      },
-                      child: Row(
-                        children: <Widget>[
-                          Icon(Icons.people_alt, color: Colors.white),
-                          Padding(padding: EdgeInsets.only(left: 16),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Text('지인등록', style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.w700,
-                                  color: Colors.white,
-                                ),)
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
+                    // height: 64, // 픽셀 에러로 삭제
+                    child: Column(
+                      children: [
+                        MaterialButton(
+                          elevation: 0.0,
+                          onPressed: (){
+                            Navigator.push(context, MaterialPageRoute(builder: (_) => HomeScreen()));
+                          },
+                          color: Color(0xFFDCEDC8),
+                          child: Icon(
+                            Icons.people_alt_rounded, color: Colors.red[300],
+                            size: 30,
+                          ),
+                          padding: EdgeInsets.all(16),
+                          shape: CircleBorder(),
+                        ),
+                        SizedBox(height: 10),
+                        Container(
+                          child: Text(
+                            '지인등록',
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.black,
+                                ),
+                        ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
@@ -71,71 +67,139 @@ class TalkPage_first extends StatelessWidget {
                   child: Container(
                     margin: EdgeInsets.only(right: 8),
                     padding: EdgeInsets.only(left: 16),
-                    height: 64,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(12),
-                      border: Border.all(color: Colors.black12, width: 1),
-                    ),
-                    child: Row(
-                      children: <Widget>[
-                        IconButton(icon: Icon(Icons.people_alt_rounded, color: Color(0xFF43aa8b),),
+                    // height: 64, // 픽셀 에러로 삭제
+                    child: Column(
+                      children: [
+                        MaterialButton(
+                          elevation: 0.0,
                           onPressed: (){
-                            Navigator.push(context, MaterialPageRoute(builder: (_)=>HomeScreen()));
-                          },),
-                        Padding(padding: EdgeInsets.only(left: 16),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Text('지인등록', style: TextStyle(
-                                color: Color(0xFF43aa8b),
-                                fontSize: 20,
-                                fontWeight: FontWeight.w700,
-                              ),)
-                            ],
+                            Navigator.push(context, MaterialPageRoute(builder: (_)=> CenHomeScreen()));
+                          },
+                          color: Color(0xFFDCEDC8),
+                          child: Icon(
+                            Icons.add_alert, color: Colors.red,
+                            size: 30,
                           ),
-                        )
+                          padding: EdgeInsets.all(16),
+                          shape: CircleBorder(),
+                        ),
+                        SizedBox(height: 10),
+                        Container(
+                          child: Text(
+                            '센터등록',
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.only(right: 8),
+                    padding: EdgeInsets.only(left: 16),
+                    // height: 64, // 픽셀 에러로 삭제
+                    child: Column(
+                      children: [
+                        MaterialButton(
+                          elevation: 0.0,
+                          onPressed: (){
+                            Navigator.push(context, MaterialPageRoute(builder: (_) => HosHomeScreen()));
+                          },
+                          color: Color(0xFFDCEDC8),
+                          child: Icon(
+                            Icons.mark_chat_unread_rounded, color: Colors.blueAccent,
+                            size: 30,
+                          ),
+                          padding: EdgeInsets.all(16),
+                          shape: CircleBorder(),
+                        ),
+                        SizedBox(height: 10),
+                        Container(
+                          child: Text(
+                            '병원등록',
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.only(right: 8),
+                    padding: EdgeInsets.only(left: 16),
+                    // height: 64, // 픽셀 에러로 삭제
+                    child: Column(
+                      children: [
+                        MaterialButton(
+                          elevation: 0.0,
+                          onPressed: (){},
+                          color: Color(0xFFDCEDC8),
+                          child: Icon(
+                            Icons.alarm_add_rounded, color: Colors.orange,
+                            size: 30,
+                          ),
+                          padding: EdgeInsets.all(16),
+                          shape: CircleBorder(),
+                        ),
+                        SizedBox(height: 10),
+                        Container(
+                          child: Text(
+                            '알람설정',
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
                       ],
                     ),
                   ),
                 ),
               ],
             ),
-            SizedBox(height: 10),
+            SizedBox(height: 30),
             Row(
               children: <Widget>[
                 Expanded(
                   child: Container(
                     margin: EdgeInsets.only(right: 8),
                     padding: EdgeInsets.only(left: 16),
-                    height: 64,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(12),
-                      border: Border.all(color: Colors.black12, width: 1),
-                    ),
-                    child: Row(
-                      children: <Widget>[
-                        IconButton(
-                          icon: Icon(Icons.pending_actions_rounded, color: Color(0xFF43aa8b)), //센터등록 아이콘 및 사진으로 변경하기
-                          onPressed: (){
-                            Navigator.push(context, MaterialPageRoute(builder: (_)=> CenHomeScreen()));
-                          }, //센터등록 페이지로 이동하기
-                        ),
-                        Padding(padding: EdgeInsets.only(left: 16),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Text('센터등록', style: TextStyle(
-                                color: Color(0xFF43aa8b),
-                                fontSize: 20,
-                                fontWeight: FontWeight.w700,
-                              ),)
-                            ],
+                    // height: 64, // 픽셀 에러로 삭제
+                    child: Column(
+                      children: [
+                        MaterialButton(
+                          elevation: 0.0,
+                          onPressed: (){},
+                          color: Color(0xFFDCEDC8),
+                          child: Icon(
+                            Icons.location_on_rounded, color: Colors.red,
+                            size: 30,
                           ),
-                        )
+                          padding: EdgeInsets.all(16),
+                          shape: CircleBorder(),
+                        ),
+                        SizedBox(height: 10),
+                        Container(
+                          child: Text(
+                            '센터찾기',
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
                       ],
                     ),
                   ),
@@ -144,51 +208,86 @@ class TalkPage_first extends StatelessWidget {
                   child: Container(
                     margin: EdgeInsets.only(right: 8),
                     padding: EdgeInsets.only(left: 16),
-                    height: 64,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(12),
-                      border: Border.all(color: Colors.black12, width: 1),
-                    ),
-                    child: Row(
-                      children: <Widget>[
-                        IconButton(onPressed: (){
-                          Navigator.push(context, MaterialPageRoute(builder: (_) => HosHomeScreen()));
-                        }, // 병원등록 홈페이지로 이동
-                            icon: Icon(Icons.local_hospital_rounded, color: Color(0xFF43aa8b),)),
-                        Padding(padding: EdgeInsets.only(left: 16),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Text('병원등록', style: TextStyle(
-                                color: Color(0xFF43aa8b),
-                                fontSize: 20,
-                                fontWeight: FontWeight.w700,
-                              ),)
-                            ],
+                    // height: 64, // 픽셀 에러로 삭제
+                    child: Column(
+                      children: [
+                        MaterialButton(
+                          elevation: 0.0,
+                          onPressed: (){},
+                          color: Color(0xFFDCEDC8),
+                          child: Icon(
+                            Icons.add_alert, color: Colors.red,
+                            size: 30,
                           ),
-                        )
+                          padding: EdgeInsets.all(16),
+                          shape: CircleBorder(),
+                        ),
+                        SizedBox(height: 10),
+                        Container(
+                          child: Text(
+                            '경보사례',
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.only(right: 8),
+                    padding: EdgeInsets.only(left: 16),
+                    // height: 64, // 픽셀 에러로 삭제
+                    child: Column(
+                      children: [
+                        MaterialButton(
+                          elevation: 0.0,
+                          onPressed: (){},
+                          color: Color(0xFFDCEDC8),
+                          child: Icon(
+                            Icons.mark_chat_unread_rounded, color: Colors.blueAccent,
+                            size: 30,
+                          ),
+                          padding: EdgeInsets.all(16),
+                          shape: CircleBorder(),
+                        ),
+                        SizedBox(height: 10),
+                        Container(
+                          child: Text(
+                            '메세지설정',
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
                       ],
                     ),
                   ),
                 ),
               ],
-            )
+            ),
+            //자가 진단 그래프 구현
+            Padding(
+              padding: EdgeInsets.only(left: 25, top: 50),
+              child: Text(
+                '자가 진단',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+
           ],
         ),
       )
     ],
-
-
-    // child: ElevatedButton(
-    //   onPressed: () {
-    //     Navigator.push(
-    //         context, MaterialPageRoute(builder: (_) =>HomeScreen()));
-    //   },
-    //   child: Text("지인등록"),
-    // )
-
-
   );
 }
