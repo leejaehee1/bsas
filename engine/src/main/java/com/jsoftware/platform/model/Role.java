5c23e820317fc57025cf139887e0475baa5b54d7
sherrygelato <sherrygelato@kakao.com>
2021-09-15 09:38:42 +0900

    BSAS-28 [+] add role

---
+++ engine/src/main/java/com/jsoftware/platform/model/Role.java
@@ -0,0 +1,13 @@
+package com.jsoftware.platform.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum Role {
+    ADMIN("ROLE_ADMIN"),
+    MEMBER("ROLE_MEMBER");
+
+    private String Value;
+}
