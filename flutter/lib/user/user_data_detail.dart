9f525d29b485de6a43fe08bdd3d056adeda82b4b
markseeun <mark233891@gmail.com>
2021-09-17 10:15:24 +0900

    BSAS-36 [+] user edit page

--- flutter/lib/user/user_data_detail.dart
+++ flutter/lib/user/user_data_detail.dart
@@ -1,109 +1,124 @@
-// import 'package:bsas/db/user_db.dart';
-// import 'package:bsas/model/user_model.dart';
-// import 'package:flutter/cupertino.dart';
-// import 'package:flutter/material.dart';
-// import 'package:bsas/model/data_model.dart';
-// import 'dart:convert';
-// import 'package:bsas/model/user_model.dart';
-// import 'package:http/http.dart' as http;
-//
-// class DataDetail extends StatefulWidget {
-//
-//   final List list;
-//   final int index;
-//
-//   DataDetail({required this.list, required this.index});
-//
-//
-//   @override
-//   State<StatefulWidget> createState() {
-//     // TODO: implement createState
-//     return DataDetailState();
-//   }
-// }
-//
-// // enum Options { delete, update }
-//
-// class DataDetailState extends State {
-//
-//   DBHelper userDataBaseHelper = DBHelper();
-//
-//   final TextEditingController _nameController = new TextEditingController();
-//   final TextEditingController _phoneController = new TextEditingController();
-//   final TextEditingController _emailController = new TextEditingController();
-//
-//   @override
-//   void initState() {
-//     // TODO: implement initState
-//     super.initState();
-//     _futureUser = getUserData();
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     // TODO: implement build
-//     return Scaffold(
-//         appBar: AppBar(
-//           backgroundColor: Color(0xFF43aa8b),
-//           title: Text(
-//             "편집",
-//             style: TextStyle(
-//                 fontWeight: FontWeight.w900, fontSize: 20, color: Colors.black),
-//           ),
-//           centerTitle: true,
-//         ),
-//         body: Container(
-//             alignment: Alignment.center,
-//             padding: const EdgeInsets.all(8.0),
-//             child: FutureBuilder<User>(
-//               future: _futureUser,
-//               builder: (context, snapshot) {
-//                 if (snapshot.connectionState == ConnectionState.done) {
-//                   if (snapshot.hasData) {
-//                     return Column(
-//                       mainAxisAlignment: MainAxisAlignment.center,
-//                       children: <Widget>[
-//                         Text(snapshot.data!.userName!),
-//                         TextField(
-//                           controller: _nameController,
-//                           decoration: const InputDecoration(
-//                             hintText: '이름을 입력하세요',
-//                           ),
-//                         ),
-//                         Text(snapshot.data!.userPhone!),
-//                         TextField(
-//                           controller: _phoneController,
-//                           decoration: const InputDecoration(
-//                             hintText: '전화번호를 입력하세요',
-//                           ),
-//                         ),
-//                         Text(snapshot.data!.userEmail!),
-//                         TextField(
-//                           controller: _nameController,
-//                           decoration: const InputDecoration(
-//                             hintText: '메일을 입력하세요',
-//                           ),
-//                         ),
-//                         ElevatedButton(
-//                           onPressed: () {
-//                             setState(() {
-//                               _futureUser = updateUser(
-//                                   _nameController.text, _phoneController.text,
-//                                   _emailController.text);
-//                             });
-//                           },
-//                           child: const Text('업데이트'),
-//                         )
-//                       ],
-//                     );
-//                   } else if (snapshot.hasError) {
-//                     return Text('${snapshot.error}');
-//                   }
-//                 }
-//                 return const CircularProgressIndicator();
-//               },
-//             )
-//         )
-//     );
-//   }
-// }
+import 'package:bsas/db/user_db.dart';
+import 'package:bsas/model/user_model.dart';
+import 'package:bsas/user/user_home_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:bsas/model/data_model.dart';
+import 'dart:convert';
+import 'package:bsas/model/user_model.dart';
+import 'package:http/http.dart' as http;
+
+class EditUser extends StatefulWidget {
+  final List list;
+  final int index;
+
+  EditUser({required this.list, required this.index});
+
+  @override
+  _EditUserState createState() => _EditUserState();
+}
+
+class _EditUserState extends State<EditUser> {
+
+  DBHelper databaseHelper = DBHelper();
+
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _phoneController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+
+  _navigateUserList(BuildContext context) async {
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => HomeScreen()));
+
+    if (result) {
+      setState(() {});
+    }
+  }
+
+  @override
+  void initState() {
+    _nameController = TextEditingController(
+        text: widget.list[widget.index]['name'].toString());
+    _phoneController = TextEditingController(
+        text: widget.list[widget.index]['price'].toString());
+    _emailController = TextEditingController(
+        text: widget.list[widget.index]['email'].toString());
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: Form(
+        child: ListView(
+          padding: const EdgeInsets.all(10.0),
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                ListTile(
+                  leading: const Icon(Icons.person, color: Colors.black),
+                  title: new TextFormField(
+                    controller: _nameController,
+                    validator: (value) {
+                      if (value!.isEmpty) return "name";
+                    },
+                    decoration: new InputDecoration(
+                      hintText: "Name",
+                      labelText: "Name",
+                    ),
+                  ),
+                ),
+                new ListTile(
+                  leading: const Icon(Icons.location_on, color: Colors.black),
+                  title: new TextFormField(
+                    controller: _phoneController,
+                    validator: (value) {
+                      if (value!.isEmpty) return "Phone";
+                    },
+                    decoration: new InputDecoration(
+                      hintText: "Phone",
+                      labelText: "Phone",
+                    ),
+                  ),
+                ),
+                new ListTile(
+                  leading: const Icon(Icons.settings_input_component,
+                      color: Colors.black),
+                  title: new TextFormField(
+                    controller: _emailController,
+                    validator: (value) {
+                      if (value!.isEmpty) return "Email";
+                    },
+                    decoration: new InputDecoration(
+                      hintText: "Email",
+                      labelText: "Email",
+                    ),
+                  ),
+                ),
+                const Divider(
+                  height: 1.0,
+                ),
+                new Padding(
+                  padding: const EdgeInsets.all(10.0),
+                ),
+                new RaisedButton(
+                  child: new Text("Edit"),
+                  color: Colors.blueAccent,
+                  onPressed: () {
+                    databaseHelper.updateUser(
+                        _nameController.text.trim(),
+                        _phoneController.text.trim(),
+                        _emailController.text.trim(),
+                    );
+                    _navigateUserList(context);
+                    },
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
