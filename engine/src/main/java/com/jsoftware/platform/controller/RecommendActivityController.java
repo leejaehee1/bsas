b2ace4e5b60186c37b18a976b74a33965b0b485d
sherrygelato <sherrygelato@kakao.com>
2021-09-23 10:20:44 +0900

    BSAS-9 [+] add img_url, add selectonebyid, add insertone

--- engine/src/main/java/com/jsoftware/platform/controller/RecommendActivityController.java
+++ engine/src/main/java/com/jsoftware/platform/controller/RecommendActivityController.java
@@ -1,9 +1,13 @@
 package com.jsoftware.platform.controller;
 
+import com.jsoftware.platform.model.EventDay;
 import com.jsoftware.platform.model.RecommendActivity;
 import com.jsoftware.platform.service.RecommendActivityService;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,10 +20,20 @@ public class RecommendActivityController {
 
     private RecommendActivityService activityService;
 
-    // test
     @GetMapping("/api/recommendActivity")
     public List<RecommendActivity> getRecommendActivity() {
         System.out.println("******GET1 register");
         return activityService.readRecommendActivities();
+    }
+
+    @GetMapping("/api/recommendActivity/{id}")
+    public RecommendActivity getRecommendActivity(@PathVariable("id") int id) {
+        System.out.println(id);
+        return activityService.readRecommendActivity(id);
+    }
+
+    @PostMapping("/api/recommendActivity")
+    public RecommendActivity createRecommendActivity(@RequestBody RecommendActivity recommendActivity) {
+        return activityService.createRecommendActivity(recommendActivity);
     }
 }
