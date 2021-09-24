53c202a8e179ec6ab1a9de53df18a7dc83b8f75d
markseeun <mark233891@gmail.com>
2021-09-24 17:49:34 +0900

    BSAS-36 [+] create recommend_activity_db

---
+++ flutter/lib/db/recommend_hotspot_db.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+import 'package:bsas/model/recommend_hotspot.dart';
+import 'package:http/http.dart' as http;
+
+class RecommendHotSpotDb{
+  Future<RecommendHotspot> getEvent() async {
+    var response = await http.get(Uri.parse("http://3.36.200.118:18080/api/url")); //주소 넣기
+    print('Response status: ${response.statusCode}');
+
+    if (response.statusCode == 400) {
+      return RecommendHotspot.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load user');
+    }
+  }
+}
