5e8fda87d979d3596f44694d839c3624b82ba77a
markseeun <mark233891@gmail.com>
2021-09-27 17:37:02 +0900

    BSAS-36 [+] modify -> todaysHeadline/eventDay db and model

--- flutter/lib/db/todays_headline_db.dart
+++ flutter/lib/db/todays_headline_db.dart
@@ -1,14 +1,14 @@
 import 'dart:convert';
-import 'package:bsas/model/recommend_hotspot.dart';
 import 'package:http/http.dart' as http;
+import 'package:bsas/model/todays_headline.dart';
 
-class TodaysHeadline{
-  Future<RecommendHotspot> getHeadline() async {
+class TodaysHeadlineDb{
+  Future<TodaysHeadline> getHeadline() async {
     var response = await http.get(Uri.parse("http://3.36.200.118:18080/api/todaysHeadline")); //주소 넣기
     print('Response status: ${response.statusCode}');
 
     if (response.statusCode == 400) {
-      return RecommendHotspot.fromJson(jsonDecode(response.body));
+      return TodaysHeadline.fromJson(jsonDecode(response.body));
     } else {
       throw Exception('Failed to load user');
     }
