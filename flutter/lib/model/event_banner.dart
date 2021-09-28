f80822f526624d5e01768eebe40d215c0cbe27a7
markseeun <mark233891@gmail.com>
2021-09-28 15:41:27 +0900

    BSAS-36 [+] create -> event day banner model

--- flutter/lib/model/event_banner.dart
+++ flutter/lib/model/event_banner.dart
@@ -1,28 +1,32 @@
 
-class EventDay{
+class Banner{
   int? id;
   String? title;
   String? img_url;
+  String? contents;
 
 
-  EventDay({
+  Banner({
     required this.title,
     required this.img_url,
+    required this.contents
   });
 
-  EventDay.withId({
+  Banner.withId({
     this.id,
     this.title,
     this.img_url,
+    this.contents,
   });
 
 
   //json 변환
-  factory EventDay.fromJson(Map<String, dynamic> json){
-    return EventDay.withId(
+  factory Banner.fromJson(Map<String, dynamic> json){
+    return Banner.withId(
       id: json['id'],
       title: json['title'],
       img_url: json['img_url'],
+      contents: json['contents']
     );
   }
 }
