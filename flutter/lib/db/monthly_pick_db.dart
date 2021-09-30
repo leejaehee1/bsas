5e60f827dc7268dfa80f5632715542671d490192
markseeun <mark233891@gmail.com>
2021-09-30 16:10:45 +0900

    BSAS-36 [+] progress -> Monthly pick

--- flutter/lib/db/monthly_pick_db.dart
+++ flutter/lib/db/monthly_pick_db.dart
@@ -2,15 +2,13 @@ import 'dart:convert';
 import 'package:bsas/model/monthly_pick.dart';
 import 'package:http/http.dart' as http;
 
-class MonthlyPickDB{
-  Future<MonthlyPick> getMonthlyPick() async {
-    var response = await http.get(Uri.parse("http://3.36.200.118:18080/api/monthlyPick")); //주소 넣기
-    print('Response status: ${response.statusCode}');
+List<MonthlyPick> parsePhotos(String responseBody) {
+  final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
+  return parsed.map<MonthlyPick>((json) => MonthlyPick.fromJson(json)).toList();
+}
 
-    if (response.statusCode == 400) {
-      return MonthlyPick.fromJson(jsonDecode(response.body));
-    } else {
-      throw Exception('Failed to load user');
-    }
-  }
+Future<List<MonthlyPick>> fetchPhotos(http.Client client) async {
+  final response =
+  await client.get(Uri.parse("http://54.180.102.153:18080/api/monthlyPick"));
+  return parsePhotos(response.body);
 }
