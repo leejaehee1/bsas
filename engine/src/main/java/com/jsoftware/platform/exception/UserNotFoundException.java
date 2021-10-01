ca0339d420e30f5b2c0c18db908176e671ada399
sherrygelato <sherrygelato@kakao.com>
2021-10-01 13:31:50 +0900

    BSAS-37 [+] update exception code

--- engine/src/main/java/com/jsoftware/platform/exception/UserNotFoundException.java
+++ engine/src/main/java/com/jsoftware/platform/exception/UserNotFoundException.java
@@ -1,5 +1,9 @@
 package com.jsoftware.platform.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(String message) {
         // 부모클래스쪽으로 전달받은 메세지를 반환
