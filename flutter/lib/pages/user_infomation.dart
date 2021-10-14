0f80b560a8218c9cea9b48c67234ab68347e6f84
markseeun <mark233891@gmail.com>
2021-10-14 16:11:37 +0900

    BSAS-27 [+] complete -> user info ui

--- flutter/lib/pages/user_infomation.dart
+++ flutter/lib/pages/user_infomation.dart
@@ -1,23 +1,117 @@
 import 'package:bsas/user_info_page/activity_page.dart';
-import 'package:bsas/user_info_page/addFriends_page.dart';
 import 'package:bsas/user_info_page/notice_page.dart';
 import 'package:bsas/user_info_page/question_page.dart';
 import 'package:bsas/user_info_page/setting_page.dart';
 import 'package:bsas/user_info_page/user_info_edit.dart';
 import 'package:flutter/material.dart';
 
-
 class UserInformation extends StatefulWidget {
+  const UserInformation({Key? key}) : super(key: key);
+
   @override
   _UserInformationState createState() => _UserInformationState();
 }
 
 class _UserInformationState extends State<UserInformation> {
 
-  Widget _text(String text){
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          SizedBox(
+            height: 220,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  ListTile(
+                      title: _text(
+                          '000님 안녕하세요'), //회원가입, 로그인 완성되면 '000님' 부분에 data get 해오기
+                      subtitle:
+                          _subText('이메일'), //회원가입, 로그인 완성되면 email data get 해오기
+                      trailing: IconButton(
+                        icon: const Icon(
+                          Icons.arrow_forward_ios_rounded,
+                          color: Color(0xFFE0E0E0),
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (_) => UserInfoEdit()));
+                        },
+                      )),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      _columnBuild('쿠폰', Icons.payments_outlined),
+                      _columnBuild('이벤트', Icons.event_available_outlined),
+                      _columnBuild('포인트', Icons.control_point_duplicate_outlined),
+                      _columnBuild('친구초대', Icons.person_add_alt_1_outlined),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+          // 고객센터 카테고리
+          SingleChildScrollView(
+            child: Card(
+              elevation: 0.0,
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    // 활동 내역
+                    ListTile(
+                        leading: _thirdText('고객센터')
+                    ),
+                    _gestureDetector('자주묻는질문', Icons.assignment_outlined, ActivityPage()), //활동내역
+                    _gestureDetector('문의사항', Icons.question_answer_outlined, const QuestionPage()), //문의사항
+                    _gestureDetector('공지사항', Icons.keyboard_voice_outlined, NoticePage()), // 공지사항
+                  ],
+                ),
+              ),
+            ),
+          ),
+          // 설정 카테고리
+          SingleChildScrollView(
+            child: Card(
+              elevation: 0.0,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    ListTile(
+                        leading: _thirdText('설정')
+                    ),
+                    _gestureDetector('앱설정', Icons.settings, SettingPage),
+                    _gestureDetector('알람설정', Icons.alarm_add_rounded, SettingPage),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    ));
+  }
+
+  // 위젯 모음
+  Widget _text(String text) {
     return Text(
       text,
-      style: TextStyle(
+      style: const TextStyle(
         color: Colors.black,
         fontSize: 20,
         fontWeight: FontWeight.w600,
@@ -25,161 +119,67 @@ class _UserInformationState extends State<UserInformation> {
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      // backgroundColor: Colors.white,
-        body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(top: 20, left: 25),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    //사용자 이미지 및 이름
-                    Row(
-                      children: [
-                        CircleAvatar(
-                         backgroundColor: Color(0xFF43aa8b),
-                          child: Icon(
-                            Icons.person,
-                            color: Colors.white,
-                          ),
-                        ),
-                        SizedBox(width: 10),
-                        Text(
-                          "박마크",
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Colors.black
-                          ),
-                        ),
-                      ],
-                    ),
-                    // 알람 버튼
-                    IconButton(onPressed: (){},
-                        icon: Icon(Icons.notifications_outlined,
-                        size: 30,
-                        color: Colors.black54,),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(height: 3),
-              // 사용자 프로필 편집 화면
-              Container(
-                height: 90,
-                padding: EdgeInsets.all(8.0),
-                width: MediaQuery.of(context).size.width *1.0,
-                child: Padding(
-                  padding: const EdgeInsets.all(5.0),
-                  child: RaisedButton(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                      side: BorderSide(color: Colors.black54)
-                    ),
-                    elevation: 0.0,
-                    color: Colors.white,
-                    // // shape: ShapeBorder,
-                    onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (_) => UserInfoEdit()));
-                    }, // 프로필 편집 페이지로 연결
-                    child: Text(
-                      '프로필 편집',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.w600
-                      ),
-                    ),
-                  )
-                ),
-              ),
-              SizedBox(height: 20),
-              // 세부 카테고리
-              Container(
-                height: 250,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Card(
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        // 활동 내역
-                        GestureDetector(
-                          onTap : (){
-                            Navigator.push(context, MaterialPageRoute(builder: (_)=> ActivityPage()));
-                          },
-                        child: ListTile(
-                          leading: Icon(Icons.assignment_outlined),
-                          title: _text('활동내역'),
-                        ),
-                      ),
-                        // 문의사항
-                        GestureDetector(
-                          onTap : (){
-                            Navigator.push(context, MaterialPageRoute(builder: (_)=> QuestionPage()));
-                          },
-                          child: ListTile(
-                            leading: Icon(Icons.question_answer_outlined),
-                            title: _text('고객센터'),
-                          ),
-                        ),
-                        // 공지사항
-                        GestureDetector(
-                          onTap : (){
-                            Navigator.push(context, MaterialPageRoute(builder: (_)=> NoticePage()));
-                          },
-                          child: ListTile(
-                            leading: Icon(Icons.keyboard_voice_outlined),
-                            title: _text('공지사항'),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height: 10),
-              // 세부 카테고리
-              Container(
-                height: 150,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Card(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        GestureDetector(
-                          onTap : (){
-                            Navigator.push(context, MaterialPageRoute(builder: (_)=> AddFriendsPage()));
-                          },
-                          child: ListTile(
-                            leading: Icon(Icons.person_add_alt_1_rounded),
-                            title: _text('친구초대'),
-                          ),
-                        ),
-                        GestureDetector(
-                          onTap : (){
-                            Navigator.push(context, MaterialPageRoute(builder: (_)=> SettingPage()));
-                          },
-                          child: ListTile(
-                            leading: Icon(Icons.settings),
-                            title: _text('앱 설정'),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ],
+  Widget _subText(String text) {
+    return Text(
+      text,
+      style: const TextStyle(
+        color: Color(0xFFE0E0E0),
+        fontSize: 15,
+        fontWeight: FontWeight.w600,
+      ),
+    );
+  }
+
+  Widget _thirdText(String text) {
+    return Text(
+      text,
+      style: const TextStyle(
+        color: Colors.grey,
+        fontSize: 15,
+        fontWeight: FontWeight.w600,
+      ),
+    );
+  }
+
+  Widget _columnBuild(String text, dynamic icon) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        IconButton(
+            onPressed: () {}, // 추후 navigate 추가해야함 (dynamic으로 추가하기)
+            icon: Icon(
+              icon,
+              size: 30,
+              color: Colors.black,
+            )),
+        Text(
+          text,
+          style: const TextStyle(
+            color: Colors.black,
+            fontSize: 15,
+            fontWeight: FontWeight.w600,
           ),
-        )
+        ),
+      ],
+    );
+  }
+  Widget _gestureDetector(String text, dynamic icon, dynamic navigate) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (_) => navigate));
+      },
+      child: ListTile(
+        leading: Icon(icon, color: Colors.black,),
+        title: Text(
+          text,
+          style: const TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
     );
   }
 }
