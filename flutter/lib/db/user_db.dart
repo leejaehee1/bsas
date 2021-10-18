6d4e728ef6ffb52cfb4cf7a1934dd43102291496
markseeun <mark233891@gmail.com>
2021-10-18 16:27:07 +0900

    modify code

--- flutter/lib/db/user_db.dart
+++ flutter/lib/db/user_db.dart
@@ -10,10 +10,10 @@ class DBHelper {
     return json.decode(response.body);
   }
 
-  Future<User> getUserData() async {
-    final response = await http.get(Uri.parse("http://54.180.102.153:18080/api/users/{id}"));
+  Future<User> getUserData(String id) async {
+    final response = await http.get(Uri.parse("http://54.180.102.153:18080/api/users" + id));
     print('Response status: ${response.statusCode}');
-    if (response.statusCode == 400) {
+    if (response.statusCode == 200) {
       return User.fromJson(jsonDecode(response.body));
     } else {
       throw Exception('Failed to load user');
